d4e797a070b8536d6d159d9b0e3a6b584886e02d
Ridwan <kadriridwan0@gmail.com>
2020-10-26 01:04:18 +0000

    added router package and withRouter HOC to menu-items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
 
+//Pages
 import Homepage from './pages/Homepage/homepage';
+
+//others
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+  <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
-    <Homepage/>
+    <Router>
+      <Switch>
+      <Route path='/hats'>
+          <HatsPage/>
+        </Route>
+        <Route path='/'>
+          <Homepage/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -8,7 +8,7 @@ const Directory = () => {
         title: 'hats',
         imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
         id: 1,
-        linkUrl: 'shop/hats'
+        linkUrl: 'hats'
       },
       {
         title: 'jackets',
@@ -41,8 +41,8 @@ const Directory = () => {
     return (
         <div className='directory-menu'>
             {
-                sections.map(({title, imageUrl, id, size}) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                sections.map(({id, ...otherProps}) => (
+                    <MenuItem key={id} {...otherProps}/>
                 ))
             }
         </div>
